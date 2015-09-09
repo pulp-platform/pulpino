8a233009d0c43ea15ecc367e6239a8feb5a81f3a
Semester Student Account <sem15h5@badile-k61-5a.ee.ethz.ch>
2015-09-09 16:49:03 +0200

    implemented and tested registers mtimecmp and mtime

--- sw/apps/timer_test/main.c
+++ sw/apps/timer_test/main.c
@@ -5,10 +5,19 @@
 
 int main()
 {
-  start_timer();
-
-  printf("Time is %d\n", get_time());
-  printf("Hello World!!!!!\n");
+	uint32_t reg, val;
+ 	//start_timer();
+	asm volatile ("csrr %0, 0x321" : "=r" (reg));
+	printf("Register content is %d\n", reg);
+	val = 55;
+	asm volatile ("csrw 0x321, %1\n"
+				  "csrr %0, 0x321\n" 
+				  : "=r" (reg) 
+				  : "r" (val)
+				 );
+	
+	printf("Register content is now %d\n", reg);
+	printf("Hello World!!!!!\n");
 
   return 0;
 }
