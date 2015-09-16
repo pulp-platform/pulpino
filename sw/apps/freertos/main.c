590876467c0c11c3b53d38a82917ea37173b8aca
Flo Zaruba <fl.zaruba@be4web.net>
2015-09-16 11:28:44 +0200

    Initial freertos commit

---
+++ sw/apps/freertos/main.c
@@ -0,0 +1,24 @@
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+int main( void )
+{
+	/* Configure the hardware ready to run the demo. */
+	//prvSetupHardware();
+
+	/* The mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting is described at the top
+	of this file. */
+	//#if( mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1 )
+	//{
+	//	main_blinky();
+	//}
+	//#else
+	//{
+	//	main_full();
+	//}
+	//#endif
+
+	return 0;
+}
