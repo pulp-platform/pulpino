9fdb84768107d7d4021173f9bb9d007776458dd5
Andreas Traber <andytraber@gmail.com>
2015-11-02 11:19:26 +0100

    Remove submodules and replace them with ips_lists.txt

--- update-ips.py
+++ update-ips.py
@@ -3,7 +3,12 @@ import sys,os,subprocess
 
 # git command to use
 git = "git"
+
+server = "git@iis-git.ee.ethz.ch:pulp-project"
+
 cwd = os.getcwd()
+
+errors = []
 
 class tcolors:
     OK      = '\033[92m'
@@ -11,20 +16,110 @@ class tcolors:
     ERROR   = '\033[91m'
     ENDC    = '\033[0m'
 
-def execute(cmd):
-    return subprocess.call(cmd.split())
+devnull = open(os.devnull, 'wb')
+
+with open("ips_list.txt", "rb") as f:
+    ips_list = f.readlines()
+
+def execute(cmd, silent=False):
+    if silent:
+        stdout = devnull
+    else:
+        stdout = None
+
+    return subprocess.call(cmd.split(), stdout=stdout)
+
+# get a list of all IPs that we are interested in from ips_list.txt
+ips = []
+for ips_el in ips_list:
+    if ips_el[0] == "#":
+        continue
+    try:
+        splits = ips_el.split()
+
+        path   = splits[0]
+        commit = splits[1]
+
+        path_split = splits[0].split('/')
+
+        name = path_split[-1]
+
+        ips.append({'name': name, 'commit': commit, 'path': path })
+    except IndexError:
+        continue
+
+# update or clone the IPs
+for ip in ips:
+    # make sure we are in the correct directory to start
+    os.chdir(cwd)
+    print
+
+    # check if directory already exists, this hints to the fact that we probably already cloned it
+    if os.path.isdir("./%s" % ip['path']):
+        os.chdir("./%s" % ip['path'])
+
+        # now check if the directory is a git directory
+        if not os.path.isdir(".git"):
+            print tcolors.ERROR + "ERROR: Found a normal directory instead of a git directory at %s. You may have to delete this folder to make this script work again" % os.getcwd() + tcolors.ENDC
+            errors.append("%s - %s: Not a git directory" % (ip['name'], ip['path']));
+            continue
+
+        print tcolors.OK + "Updating %s..." % ip['name'] + tcolors.ENDC
+
+        # fetch everything first so that all commits are available later
+        ret = execute("%s fetch" % (git))
+        if ret != 0:
+            print tcolors.ERROR + "ERROR: could not fetch IP %s." % (ip['name']) + tcolors.ENDC
+            errors.append("%s - Could not fetch" % (ip['name']));
+            continue
+
+        # make sure we have the correct branch/tag for the pull
+        ret = execute("%s checkout %s" % (git, ip['commit']))
+        if ret != 0:
+            print tcolors.ERROR + "ERROR: could not checkout IP %s at %s." % (ip['name'], ip['commit']) + tcolors.ENDC
+            errors.append("%s - Could not checkout commit %s" % (ip['name'], ip['commit']));
+            continue
+
+        # check if we are in detached HEAD mode
+        ret = execute("%s status | grep \"HEAD detached\"" % (git), True)
+        if ret != 0:
+            # only do the pull if we are not in detached head mode
+            ret = execute("%s pull --ff-only" % git)
+            if ret != 0:
+                print tcolors.ERROR + "ERROR: could not update %s" % ip['name'] + tcolors.ENDC
+                errors.append("%s - Could not update" % (ip['name']));
+                continue
+
+    # Not yet cloned, so we have to do that first
+    else:
+        os.chdir("./")
+        ret = execute("%s clone %s/%s.git %s" % (git, server, ip['name'], ip['path']))
+        if ret != 0:
+            print tcolors.ERROR + "ERROR: could not clone, you probably have to remove the %s directory." % ip['name'] + tcolors.ENDC
+            errors.append("%s - Could not clone" % (ip['name']));
+            continue
+
+        os.chdir("./%s" % ip['path'])
+        ret = execute("%s checkout %s" % (git, ip['commit']))
+        if ret != 0:
+            print tcolors.ERROR + "ERROR: could not checkout IP %s at %s." % (ip['name'], ip['commit']) + tcolors.ENDC
+            errors.append("%s - Could not checkout commit %s" % (ip['name'], ip['commit']));
+            continue
+
+os.chdir(cwd)
 
 
-ret = execute("%s submodule init" % git)
+print '\n\n'
+print tcolors.WARNING + "SUMMARY" + tcolors.ENDC
 
-if ret != 0:
-    print tcolors.ERROR + "ERROR: could not init submodules"
+if len(errors) == 0:
+    print tcolors.OK + "IPs updated successfully!" + tcolors.ENDC
+    sys.exit(0)
+else:
+    for error in errors:
+        print tcolors.ERROR + '    %s' % (error) + tcolors.ENDC
+
+    print
+
+    print tcolors.ERROR + "ERRORS during IP update!" + tcolors.ENDC
     sys.exit(1)
-
-ret = execute("%s submodule update" % git)
-if ret != 0:
-    print tcolors.ERROR + "ERROR: could not update submodules"
-    sys.exit(1)
-
-
-sys.exit(0)
