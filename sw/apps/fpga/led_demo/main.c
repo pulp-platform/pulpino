94d36d6827db90ae2cb2730a3f345872b88b4287
Andreas Traber <andytraber@gmail.com>
2015-12-17 23:19:41 +0100

    Add LED demo

---
+++ sw/apps/fpga/led_demo/main.c
@@ -0,0 +1,157 @@
+
+#include "string_lib.h"
+#include "utils.h"
+#include "gpio.h"
+#include "uart.h"
+
+void left_to_right_single(unsigned int first, unsigned int last);
+void left_to_right_on(unsigned int first, unsigned int last);
+void left_to_right_off(unsigned int first, unsigned int last);
+void left_to_right_on_slow(unsigned int first, unsigned int last);
+void left_to_right_off_slow(unsigned int first, unsigned int last);
+
+void waste_time() {
+  int i;
+  for(i = 0; i < 100000; i++) asm volatile("nop");
+}
+
+#define SW_0_BIT    (1 <<  0)
+#define SW_1_BIT    (1 <<  1)
+#define SW_2_BIT    (1 <<  2)
+#define SW_3_BIT    (1 <<  3)
+#define SW_4_BIT    (1 <<  4)
+#define SW_5_BIT    (1 <<  5)
+#define SW_6_BIT    (1 <<  6)
+#define SW_7_BIT    (1 <<  7)
+
+#define LED_0        8
+#define LED_1        9
+#define LED_2       10
+#define LED_3       11
+#define LED_4       12
+#define LED_5       13
+#define LED_6       14
+#define LED_7       15
+
+#define BTN_C_BIT   (1 << 16)
+#define BTN_D_BIT   (1 << 17)
+#define BTN_L_BIT   (1 << 18)
+#define BTN_R_BIT   (1 << 19)
+#define BTN_U_BIT   (1 << 20)
+
+int main()
+{
+  int i;
+
+  for(i = 0; i < 8; i++) {
+    set_gpio_pin_direction(i, DIR_IN);
+    set_pin_function(i, FUNC_GPIO);
+  }
+
+  for(i = 8; i < 16; i++) {
+    set_gpio_pin_direction(i, DIR_OUT);
+    set_pin_function(i, FUNC_GPIO);
+  }
+
+  for(i = 16; i < 20; i++) {
+    set_gpio_pin_direction(i, DIR_IN);
+    set_pin_function(i, FUNC_GPIO);
+  }
+
+  while(1) {
+    left_to_right_on_slow(LED_1, LED_7);
+    left_to_right_off_slow(LED_1, LED_7);
+
+    waste_time();
+
+    left_to_right_on(LED_1, LED_7);
+    left_to_right_off(LED_1, LED_7);
+
+    waste_time();
+
+    left_to_right_single(LED_1, LED_7);
+    left_to_right_single(LED_1, LED_7);
+    left_to_right_single(LED_1, LED_7);
+  }
+
+  return 0;
+}
+
+void left_to_right_single(unsigned int first, unsigned int last) {
+  unsigned int i;
+  unsigned int j;
+
+  for(i = first; i <= last; i++) {
+
+    for(j = first; j <= last; j++) {
+      set_gpio_pin_value(j, 0);
+    }
+
+    set_gpio_pin_value(i, 1);
+
+    waste_time();
+  }
+}
+
+void left_to_right_on(unsigned int first, unsigned int last) {
+  unsigned int i;
+  unsigned int j;
+
+
+  for(j = first; j <= last; j++) {
+    set_gpio_pin_value(j, 0);
+  }
+
+  for(i = first; i <= last; i++) {
+
+    set_gpio_pin_value(i, 1);
+
+    waste_time();
+  }
+}
+
+void left_to_right_off(unsigned int first, unsigned int last) {
+  unsigned int i;
+  unsigned int j;
+
+
+  for(j = first; j <= last; j++) {
+    set_gpio_pin_value(j, 1);
+  }
+
+  for(i = first; i <= last; i++) {
+
+    set_gpio_pin_value(i, 0);
+
+    waste_time();
+  }
+}
+
+void left_to_right_on_slow(unsigned int first, unsigned int last) {
+  int i;
+  int j;
+
+  for(j = first; j <= last; j++) {
+    set_gpio_pin_value(j, 0);
+  }
+
+  for(i = last; i > first; i--) {
+
+    left_to_right_single(first, i);
+  }
+}
+
+void left_to_right_off_slow(unsigned int first, unsigned int last) {
+  int i;
+  int j;
+
+  for(j = first; j <= last; j++) {
+    set_gpio_pin_value(j, 1);
+  }
+
+  for(i = last; i >= first; i--) {
+
+    left_to_right_single(i, last);
+  }
+}
+
