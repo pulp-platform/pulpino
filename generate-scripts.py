027629f8e56d40c96cf76c918103e0def5ff77a7
Andreas Traber <andytraber@gmail.com>
2016-03-24 09:49:53 +0100

    Start to adapt to new fpga synthesis flow

--- generate-scripts.py
+++ generate-scripts.py
@@ -40,12 +40,20 @@ execute("mkdir -p vsim/vcompile/ips")
 execute("rm -rf vsim/vcompile/ips/*")
 
 # creates an IPApproX database
-ipdb = ipstools.IPDatabase(ips_dir="./ips", vsim_dir="./vsim")
+ipdb = ipstools.IPDatabase(ips_dir="./ips", rtl_dir="./rtl", vsim_dir="./vsim")
 # generate ModelSim/QuestaSim compilation scripts
 ipdb.export_vsim(script_path="vsim/vcompile/ips", target_tech='umc65')
 # generate vsim.tcl with ModelSim/QuestaSim "linking" script
 ipdb.generate_vsim_tcl("vsim/tcl_files/config/vsim_ips.tcl")
 # generate script to compile all IPs for ModelSim/QuestaSim
 ipdb.generate_vcompile_libs_csh("vsim/vcompile/vcompile_ips.csh")
+
+# generate Vivado compilation scripts
+ipdb.export_vivado(script_path="fpga/pulpino/tcl/ips_src_files.tcl", alternatives=['riscv'])
+# generate Vivado add_files.tcl
+ipdb.generate_vivado_add_files("fpga/pulpino/tcl/ips_add_files.tcl", alternatives=['riscv'])
+# generate Vivado inc_dirs.tcl
+ipdb.generate_vivado_inc_dirs("fpga/pulpino/tcl/ips_inc_dirs.tcl", alternatives=['riscv'])
+
 
 print tcolors.OK + "Generated new scripts for IPs!" + tcolors.ENDC
