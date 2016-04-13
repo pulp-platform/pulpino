efedea80aabcd1ef2c655607ccd4b2b03b3df7ec
Andreas Traber <andytraber@gmail.com>
2016-04-13 15:19:05 +0200

    Add testMUL which checks mulh, mulhu, mulhsu

---
+++ sw/apps/riscv_tests/testMUL/testMUL.c
@@ -0,0 +1,113 @@
+// Copyright 2015 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the “License”); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+
+#include <stdio.h>
+#include "utils.h"
+#include "bench.h"
+
+void check_mulh  (testresult_t *result, void (*start)(), void (*stop)());
+void check_mulhu (testresult_t *result, void (*start)(), void (*stop)());
+void check_mulhsu(testresult_t *result, void (*start)(), void (*stop)());
+
+testcase_t testcases[] = {
+  { .name = "mulh",          .test = check_mulh          },
+  { .name = "mulhu",         .test = check_mulhu         },
+  { .name = "mulhsu",        .test = check_mulhsu        },
+  {0, 0}
+};
+
+int main()
+{
+  if(get_core_id() == 0) {
+    return run_suite(testcases);
+  }
+
+  return 0;
+}
+
+#define util_check_mulh(result, a_const, b_const, exp) \
+    a = a_const; \
+    b = b_const; \
+    asm volatile ("mulh %[c], %[a], %[b];" \
+                  : [c] "=r" (act) \
+                  : [a] "r"  (a), \
+                    [b] "r"  (b)); \
+    check_uint32(result, "mulh", act, exp);
+
+#define util_check_mulhu(result, a_const, b_const, exp) \
+    a = a_const; \
+    b = b_const; \
+    asm volatile ("mulhu %[c], %[a], %[b];" \
+                  : [c] "=r" (act) \
+                  : [a] "r"  (a), \
+                    [b] "r"  (b)); \
+    check_uint32(result, "mulhu", act, exp);
+
+#define util_check_mulhsu(result, a_const, b_const, exp) \
+    a = a_const; \
+    b = b_const; \
+    asm volatile ("mulhsu %[c], %[a], %[b];" \
+                  : [c] "=r" (act) \
+                  : [a] "r"  (a), \
+                    [b] "r"  (b)); \
+    check_uint32(result, "mulhsu", act, exp);
+
+
+void check_mulh(testresult_t *result, void (*start)(), void (*stop)()) {
+  int act;
+  int a;
+  int b;
+
+  util_check_mulh(result, 0x0, 0x0, 0x0);
+  util_check_mulh(result, 0x00010000, 0x00010000, 0x00000001);
+  util_check_mulh(result, 0x00010000, 0x00000000, 0x00000000);
+  util_check_mulh(result, 0x00000000, 0x00010000, 0x00000000);
+  util_check_mulh(result, 0x00000001, 0x00000000, 0x00000000);
+  util_check_mulh(result, 0x00000000, 0x00000001, 0x00000000);
+  util_check_mulh(result, 0x10000000, 0x10000000, 0x01000000);
+  util_check_mulh(result, 0xFFFFFFFF, 0xFFFFFFFF, 0x00000000);
+  util_check_mulh(result, 0xFFFFFFFF, 0x00000001, 0xFFFFFFFF);
+  util_check_mulh(result, 0x00000001, 0xFFFFFFFF, 0xFFFFFFFF);
+}
+
+void check_mulhu(testresult_t *result, void (*start)(), void (*stop)()) {
+  int act;
+  int a;
+  int b;
+
+  util_check_mulhu(result, 0x0, 0x0, 0x0);
+  util_check_mulhu(result, 0x00010000, 0x00010000, 0x00000001);
+  util_check_mulhu(result, 0x00010000, 0x00000000, 0x00000000);
+  util_check_mulhu(result, 0x00000000, 0x00010000, 0x00000000);
+  util_check_mulhu(result, 0x00000001, 0x00000000, 0x00000000);
+  util_check_mulhu(result, 0x00000000, 0x00000001, 0x00000000);
+  util_check_mulhu(result, 0x10000000, 0x10000000, 0x01000000);
+  util_check_mulhu(result, 0xFFFFFFFF, 0xFFFFFFFF, 0xfffffffe);
+  util_check_mulhu(result, 0xFFFFFFFF, 0x00000001, 0x00000000);
+  util_check_mulhu(result, 0x00000001, 0xFFFFFFFF, 0x00000000);
+}
+
+void check_mulhsu(testresult_t *result, void (*start)(), void (*stop)()) {
+  int act;
+  int a;
+  int b;
+
+  util_check_mulhsu(result, 0x0, 0x0, 0x0);
+  util_check_mulhsu(result, 0x00010000, 0x00010000, 0x00000001);
+  util_check_mulhsu(result, 0x00010000, 0x00000000, 0x00000000);
+  util_check_mulhsu(result, 0x00000000, 0x00010000, 0x00000000);
+  util_check_mulhsu(result, 0x00000001, 0x00000000, 0x00000000);
+  util_check_mulhsu(result, 0x00000000, 0x00000001, 0x00000000);
+  util_check_mulhsu(result, 0x10000000, 0x10000000, 0x01000000);
+  util_check_mulhsu(result, 0xFFFFFFFF, 0xFFFFFFFF, 0xffffffff);
+  util_check_mulhsu(result, 0xFFFFFFFF, 0x00000001, 0xffffffff);
+  util_check_mulhsu(result, 0x00000001, 0xFFFFFFFF, 0x00000000);
+}
