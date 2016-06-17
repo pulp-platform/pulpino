384abf77b9140fd4024a570a818356ec88740950
Mohr <mahmoud.elmohr@gmail.com>
2016-06-17 04:05:22 +0200

    solving interrupt apps dependencies (Attempt 1)

--- sw/apps/riscv_tests/testEvents/testEvents.c
+++ sw/apps/riscv_tests/testEvents/testEvents.c
@@ -122,8 +122,8 @@ void check_sleep_irq(testresult_t *result, void (*start)(), void (*stop)()) {
   int_disable();
 }
 
-__attribute__ ((interrupt))
-void int_time_cmp(void) {
+
+void ISR_TA_OVF(void) {
   ICP = (1 << 29);
 
   switch (g_sleep_irq_global) {
