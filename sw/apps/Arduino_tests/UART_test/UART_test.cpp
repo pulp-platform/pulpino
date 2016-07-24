56e989b9d6376f9da6674471fdc6036c6098de81
Mohr <mahmoud.elmohr@gmail.com>
2016-07-24 23:27:46 +0200

    Verified UART reciever

--- sw/apps/Arduino_tests/UART_test/UART_test.cpp
+++ sw/apps/Arduino_tests/UART_test/UART_test.cpp
@@ -1,68 +1,61 @@
+//Output shoud be :
+// 0  intialized in the rx 
+// 101 asserted to tx by the test bench
+// 7 intialized for the variable (no thing to read)
+
+
 #include "main.cpp"
 
-int thisByte = '!';
+byte thisByte[3] = {5, 6, 7};
+volatile int x;
 
-/*
-  ASCII table
-
- Prints out byte values in all possible formats:
- * as raw binary values
- * as ASCII-encoded decimal, hex, octal, and binary values
-
- For more on ASCII, see http://www.asciitable.com and http://en.wikipedia.org/wiki/ASCII
-
- The circuit:  No external hardware needed.
-
- created 2006
- by Nicholas Zambetti
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- <http://www.zambetti.com>
-
- */
 void setup() {
-  //Initialize serial and wait for port to open:
+  
+  pinMode(0,OUTPUT);
   Serial.begin(781250);
   
-  Serial.println("ASCII Table ~ Character Map");
+  digitalWrite(0,HIGH);
 
-  
+  delayMicroseconds(100);
+
+  for (x=0;x<3;x++){
+	  if (Serial.available()){
+	  	thisByte[x]=Serial.read();
+	  } 
+  }
+
+  for (x=0;x<3;x++){
+
+  	  Serial.write(thisByte[x]);
+
+	  Serial.print(", dec: ");
+	  // prints value as string as an ASCII-encoded decimal (base 10).
+	  // Decimal is the  default format for Serial.print() and Serial.println(),
+	  // so no modifier is needed:
+	  Serial.print(thisByte[x]);
+	  // But you can declare the modifier for decimal if you want to.
+	  //this also works if you uncomment it:
+
+	  // Serial.print(thisByte, DEC);
+
+
+	  Serial.print(", hex: ");
+	  // prints value as string in hexadecimal (base 16):
+	  Serial.print(thisByte[x], HEX);
+
+	  Serial.print(", oct: ");
+	  // prints value as string in octal (base 8);
+	  Serial.print(thisByte[x], OCT);
+
+	  Serial.print(", bin: ");
+	  // prints value as string in binary (base 2)
+	  // also prints ending line break:
+	  Serial.println(thisByte[x], BIN);
+  }
+	 
 }
 
 void loop(){
+x++;
 
-  thisByte=Serial.read();
-  
-  // prints value unaltered, i.e. the raw binary version of the
-  // byte. The serial monitor interprets all bytes as
-  // ASCII, so 33, the first number,  will show up as '!'
-  Serial.write(thisByte);
-
-  Serial.print(", dec: ");
-  // prints value as string as an ASCII-encoded decimal (base 10).
-  // Decimal is the  default format for Serial.print() and Serial.println(),
-  // so no modifier is needed:
-  Serial.print(thisByte);
-  // But you can declare the modifier for decimal if you want to.
-  //this also works if you uncomment it:
-
-  // Serial.print(thisByte, DEC);
-
-
-  Serial.print(", hex: ");
-  // prints value as string in hexadecimal (base 16):
-  Serial.print(thisByte, HEX);
-
-  Serial.print(", oct: ");
-  // prints value as string in octal (base 8);
-  Serial.print(thisByte, OCT);
-
-  Serial.print(", bin: ");
-  // prints value as string in binary (base 2)
-  // also prints ending line break:
-  Serial.println(thisByte, BIN);
- 
 }
---
+++ sw/apps/Arduino_tests/UART_test/tb.sv
@@ -0,0 +1,255 @@
+`include "config.sv"
+`include "tb_jtag_pkg.sv"
+
+`define REF_CLK_PERIOD   (2*15.25us)  // 32.786 kHz --> FLL reset value --> 50 MHz
+`define CLK_PERIOD       40.00ns      // 25 MHz
+
+`define EXIT_SUCCESS  0
+`define EXIT_FAIL     1
+`define EXIT_ERROR   -1
+
+module tb;
+  timeunit      1ns;
+  timeprecision 1ps;
+
+  // +MEMLOAD= valid values are "SPI", "STANDALONE" "PRELOAD", "" (no load of L2)
+  parameter  SPI           = "QUAD";    // valid values are "SINGLE", "QUAD"
+  parameter  BAUDRATE      = 781250; // 1562500
+  parameter  CLK_USE_FLL   = 0;  // 0 or 1
+  parameter  TEST          = ""; //valid values are "" (NONE), "DEBUG"
+
+  parameter  RX_BIT_PERIOD = (1000000000/BAUDRATE);
+
+  int           exit_status = `EXIT_ERROR; // modelsim exit code, will be overwritten when successful
+
+  string        memload;
+  logic         s_clk   = 1'b0;
+  logic         s_rst_n = 1'b0;
+
+  logic         fetch_enable = 1'b0;
+
+  logic [1:0]   padmode_spi_master;
+  logic         spi_sck   = 1'b0;
+  logic         spi_csn   = 1'b1;
+  logic [1:0]   spi_mode;
+  logic         spi_sdo0;
+  logic         spi_sdo1;
+  logic         spi_sdo2;
+  logic         spi_sdo3;
+  logic         spi_sdi0;
+  logic         spi_sdi1;
+  logic         spi_sdi2;
+  logic         spi_sdi3;
+
+  logic         uart_tx;
+  logic         uart_rx;
+  logic         s_uart_dtr;
+  logic         s_uart_rts;
+
+  logic [31:0]  gpio_out;
+  logic [31:0]  gpio_in;
+  logic [31:0]  gpio_dir;
+
+  logic [31:0]  recv_data;
+
+  jtag_i jtag_if();
+
+  adv_dbg_if_t adv_dbg_if = new(jtag_if);
+
+  // use 8N1
+  uart_tb_rx
+  #(
+    .BAUD_RATE(BAUDRATE),
+    .PARITY_EN(0)
+  )
+  uart_tb_rx_i
+  (
+    .rx(uart_rx),
+    .rx_en(1'b1),
+    .word_done()
+  );
+
+
+  pulpino_top top_i
+  (
+    .clk               ( s_clk        ),
+    .rst_n             ( s_rst_n      ),
+
+    .clk_sel_i         ( 1'b0         ),
+    .testmode_i        ( 1'b0         ),
+    .fetch_enable_i    ( fetch_enable ),
+
+    .spi_clk_i         ( spi_sck      ),
+    .spi_cs_i          ( spi_csn      ),
+    .spi_mode_o        ( spi_mode     ),
+    .spi_sdo0_o        ( spi_sdi0     ),
+    .spi_sdo1_o        ( spi_sdi1     ),
+    .spi_sdo2_o        ( spi_sdi2     ),
+    .spi_sdo3_o        ( spi_sdi3     ),
+    .spi_sdi0_i        ( spi_sdo0     ),
+    .spi_sdi1_i        ( spi_sdo1     ),
+    .spi_sdi2_i        ( spi_sdo2     ),
+    .spi_sdi3_i        ( spi_sdo3     ),
+
+    .spi_master_clk_o  (              ),
+    .spi_master_csn0_o (              ),
+    .spi_master_csn1_o (              ),
+    .spi_master_csn2_o (              ),
+    .spi_master_csn3_o (              ),
+    .spi_master_mode_o (              ),
+    .spi_master_sdo0_o (              ),
+    .spi_master_sdo1_o (              ),
+    .spi_master_sdo2_o (              ),
+    .spi_master_sdo3_o (              ),
+    .spi_master_sdi0_i (              ),
+    .spi_master_sdi1_i (              ),
+    .spi_master_sdi2_i (              ),
+    .spi_master_sdi3_i (              ),
+
+    .scl_pad_i         (              ),
+    .scl_pad_o         (              ),
+    .scl_padoen_o      (              ),
+    .sda_pad_i         (              ),
+    .sda_pad_o         (              ),
+    .sda_padoen_o      (              ),
+
+
+    .uart_tx           ( uart_rx      ),
+    .uart_rx           ( uart_tx      ),
+    .uart_rts          ( s_uart_rts   ),
+    .uart_dtr          ( s_uart_dtr   ),
+    .uart_cts          ( 1'b0         ),
+    .uart_dsr          ( 1'b0         ),
+
+    .gpio_in           ( gpio_in      ),
+    .gpio_out          ( gpio_out     ),
+    .gpio_dir          ( gpio_dir     ),
+    .gpio_padcfg       (              ),
+
+    .tck_i             ( jtag_if.tck     ),
+    .trstn_i           ( jtag_if.trstn   ),
+    .tms_i             ( jtag_if.tms     ),
+    .tdi_i             ( jtag_if.tdi     ),
+    .tdo_o             ( jtag_if.tdo     )
+  );
+
+  generate
+    if (CLK_USE_FLL) begin
+      initial
+      begin
+        #(`REF_CLK_PERIOD/2);
+        s_clk = 1'b1;
+        forever s_clk = #(`REF_CLK_PERIOD/2) ~s_clk;
+      end
+    end else begin
+      initial
+      begin
+        #(`CLK_PERIOD/2);
+        s_clk = 1'b1;
+        forever s_clk = #(`CLK_PERIOD/2) ~s_clk;
+      end
+    end
+  endgenerate
+
+  logic use_qspi;
+
+  initial
+  begin
+    gpio_in=32'b0;
+
+    if(!$value$plusargs("MEMLOAD=%s", memload))
+      memload = "PRELOAD";
+
+    $display("Using MEMLOAD method: %s", memload);
+
+    use_qspi = SPI == "QUAD" ? 1'b1 : 1'b0;
+
+    s_rst_n      = 1'b0;
+    fetch_enable = 1'b0;
+
+    #500ns;
+
+    s_rst_n = 1'b1;
+
+    #500ns;
+    if (use_qspi)
+      spi_enable_qpi();
+
+
+    if (memload != "STANDALONE")
+    begin
+      /* Configure JTAG and set boot address */
+      adv_dbg_if.jtag_reset();
+      adv_dbg_if.jtag_softreset();
+      adv_dbg_if.init();
+      adv_dbg_if.axi4_write32(32'h1A10_7008, 1, 32'h0000_0000);
+    end
+
+    if (memload == "PRELOAD")
+    begin
+      // preload memories
+      mem_preload();
+    end
+    else if (memload == "SPI")
+    begin
+      spi_load(use_qspi);
+      spi_check(use_qspi);
+    end
+
+    #200ns;
+    fetch_enable = 1'b1;
+    
+    if(TEST == "DEBUG")
+    begin
+      debug_tests();
+    end else if (TEST == "MEM_DPI") begin
+      mem_dpi(4567);
+    end
+
+    
+    // Here  test for shift Starts
+    wait(top_i.gpio_out[0]);
+   
+    //tb tx is connected to pulpino rx (I'm writing to the receiver)			
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b0;	//stat bit 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b1;	//bit 0 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b0;	//bit 1 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b1;	//bit 2 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b0;	//bit 3 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b0;	//bit 4 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b1;	//bit 5 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b1;	//bit 6 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b0;	//bit 7 
+    #(RX_BIT_PERIOD);
+    uart_tx = 1'b1;	//stop bit 
+               
+    #2000000ns;
+    $stop();
+    // Here  test for shift Ends
+
+
+    // end of computation
+    wait(top_i.gpio_out[8]);
+
+    spi_check_return_codes(exit_status);
+
+    $fflush();
+    $stop();
+  end
+
+  // TODO: this is a hack, do it properly!
+  `include "tb_spi_pkg.sv"
+  `include "tb_mem_pkg.sv"
+  `include "spi_debug_test.svh"
+  `include "mem_dpi.svh"
+
+endmodule
