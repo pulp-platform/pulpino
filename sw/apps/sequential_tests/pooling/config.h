ac1d6a0523544ffcbe127a8448448d65311214ea
Pasquale Davide Schiavone <pschiavo@maipo.ee.ethz.ch>
2017-03-16 13:22:23 +0100

    added pooling in seq tests

---
+++ sw/apps/sequential_tests/pooling/config.h
@@ -0,0 +1,12 @@
+#ifndef _CONFIG_CONV_
+#define _CONFIG_CONV_
+
+#define DATA_WIDTH 16
+#define DATA_TYPE 16
+#define IMG_ROW 32
+#define IMG_COL 32
+#define IMG_DIM IMG_ROW*IMG_COL
+
+#define OUT_DIM IMG_ROW*IMG_COL/4
+
+#endif
