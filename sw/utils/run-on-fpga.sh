a756ba1274a5da7e564ac450329a2735add8cea4
Gautschi <gautschi@iis.ee.ethz.ch>
2017-03-28 17:06:39 +0200

    updated pulpino fpga folder with new busybox configuration (which supports rx command) for easier file transmission using minicom.

--- sw/utils/run-on-fpga.sh
+++ sw/utils/run-on-fpga.sh
@@ -2,7 +2,7 @@
 
 NAME=$1
 
-scp slm_files/spi_stim.txt $FPGA_HOSTNAME:/root/$NAME.spi
-ssh -t $FPGA_HOSTNAME /root/spiload --timeout=60 /root/$NAME.spi
+scp slm_files/spi_stim.txt root@$FPGA_HOSTNAME:/root/$NAME.spi
+ssh -t root@$FPGA_HOSTNAME /root/spiload --timeout=60 /root/$NAME.spi
 
 exit 0
