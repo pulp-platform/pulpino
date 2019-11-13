d728356e15b6c227e49cd033a53d51f862f641e4
Stefan Tauner <stauner@ecs.tuwien.ac.at>
2019-11-13 13:08:27 +0100

    fpga/sw: make spiload and copy it to buildroot's fs

--- fpga/sw/buildroot-post-build.sh
+++ fpga/sw/buildroot-post-build.sh
@@ -2,7 +2,7 @@
 
 # Print out dependencies for and relative to the makefile in fpga/sw
 if [ "x$1" = "xDEPENDENCIES" ]; then
-    echo ""
+    echo "apps/spiload/spiload"
     exit 0
 fi
 
@@ -10,3 +10,5 @@ fi
 mkdir -p "${1}/root/.ssh/"
 [ -f ~/.ssh/authorized_keys ] && cp ~/.ssh/authorized_keys "${1}/root/.ssh/"
 
+# spiload
+cp -a ../apps/spiload/spiload "${1}/root/"
